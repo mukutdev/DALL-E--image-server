a0821d2445e7f9c6f702026d8e0e246890c7d828
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-02-20 11:58:41 +0600

    server-initialized

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json({limit : '50mb'}))
+
+app.get('/' , async(req , res)=>{
+    res.send("hello open ai")
+})
+
+const startServer = async ()=>{
+    app.listen(8080 , ()=>{
+        console.log('Server has started on port 8080');
+    })
+}
+
+startServer()
