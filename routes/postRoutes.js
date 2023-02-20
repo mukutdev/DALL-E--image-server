5af1b23fcc401c9c5b66edf2d6ea554d0a90af58
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-02-20 19:36:38 +0600

    cloduinary-initilized

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,5 +8,34 @@ dotenv.config()
 
 const router = express.Router()
 
+cloudinary.config({
+    cloud_name : process.env.CL_CLOUD_NAME,
+    api_key : process.env.CL_CLOUD_API,
+    api_secret : process.env.CL_CLOUD_API_SECRET,
+})
+
+
+//get all
+
+
+//create post
+
+router.route('/').post(async(req,res)=>{
+    try {
+        const {name , photo , prompt} = req.body
+        const photoUrl =await cloudinary.uploader.upload(photo)
+        const newPost = await post.create({
+            name,
+            prompt,
+            photo : photoUrl.url,
+        })
+        res.status(201).json({ success : true , data : newPost})
+        
+    } catch (error) {
+
+        res.status(500).json({success : false , message : error})
+        
+    }
+})
 
 export default router;
